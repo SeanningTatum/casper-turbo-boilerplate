f6017e620434fdb169c5c6a51fd7b6f213815c6b
Sean Urgel <developer@seanurgel.dev>
2024-01-16 12:10:41 -0500

    feat: setup database prisma

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from "@ui/components/button";
+import { Checkbox } from "@ui/components/checkbox";
 
 export default function Page() {
   return (
     <>
       <h1>Web</h1>
       <Button>Click me</Button>
+      <Checkbox></Checkbox>
     </>
   );
 }
