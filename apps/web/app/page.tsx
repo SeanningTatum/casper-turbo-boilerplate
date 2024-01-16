1050a87f4968d8076e878eec06ce8c5703413e9f
Sean Urgel <developer@seanurgel.dev>
2024-01-16 12:27:25 -0500

    fix: config updates

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -3,10 +3,10 @@ import { Checkbox } from "@ui/components/checkbox";
 
 export default function Page() {
   return (
-    <>
+    <div className="h-screen">
       <h1>Web</h1>
       <Button>Click me</Button>
       <Checkbox></Checkbox>
-    </>
+    </div>
   );
 }
