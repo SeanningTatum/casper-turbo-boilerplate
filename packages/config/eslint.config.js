d8d715bc3789cfa8e7778dd06e88591c625836d4
Sean Urgel <developer@seanurgel.dev>
2024-01-16 22:35:12 -0500

    chore(config): update eslint.config.js

--- packages/config/eslint.config.js
+++ packages/config/eslint.config.js
@@ -1,12 +1,37 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "next",
+    "next/core-web-vitals",
+    "turbo",
+    "prettier",
+    "@kensho-technologies/eslint-config",
+  ],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/space-before-function-paren": "off",
+    "@typescript-eslint/indent": "off",
+    // 'react/prop-types': [true, { ignore: 'className' }],
+    "@typescript-eslint/consistent-type-definitions": "off",
+    "@typescript-eslint/strict-boolean-expressions": "off",
+    "react/destructuring-assignment": "off",
+    "@typescript-eslint/consistent-type-assertions": "off",
+    "react/jsx-no-constructed-context-values": "warn",
+    "@typescript-eslint/promise-function-async": "off",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "prefer-destructuring": "off",
   },
   parserOptions: {
     babelOptions: {
       presets: [require.resolve("next/babel")],
     },
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   settings: {
     next: {
